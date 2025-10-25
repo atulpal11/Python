253dd30068fa0071edc3ddd642cebd1bb0e54953
atulpal11 <atulpal8530@gmail.com>
2025-10-25 12:32:42 +0530

    Concatination

---
+++ ac1.py
@@ -0,0 +1,8 @@
+# num1=int(input("Enter the first number:"))
+# num2=int(input("Enter the second number:"))
+# sum = num1+num2
+# print("Sum:",sum)
+
+Side=int(input("Enter the length of side of square:"))
+Area=Side**2
+print("Area:",Area)
---
+++ ac2.py
@@ -0,0 +1,4 @@
+str1="Atul "
+str2="Pal"
+final_str=(str1+str2)
+print(final_str)
