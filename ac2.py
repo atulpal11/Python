7696eeb49a2cac8391a37862fbef2af055fa3c27
atulpal11 <atulpal8530@gmail.com>
2025-10-26 18:31:28 +0530

    Replace function

--- ac2.py
+++ ac2.py
@@ -1,4 +1,19 @@
-str1="Atul "
-str2="Pal"
-final_str=(str1+str2)
-print(final_str)
+# str1="Atul "
+# str2="Pal"
+# final_str=(str1+str2)
+# print(final_str)
+# print(len(final_str))
+# str="123456789"
+# print(str[3:-2])
+
+#String Functions
+
+str="i am coder"
+print(str.endswith("er"))   #True
+print(str.capitalize())    #I am coder
+print(str)  #i am coder
+str=str.capitalize()
+print(str)  #I am coder
+print(str)  #I am coder
+str1="I will be a great engineer"
+print(str1.replace("e","o"))
